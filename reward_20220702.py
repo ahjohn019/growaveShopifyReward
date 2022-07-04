a115cfd4f536aa5758a0c87c2ae2e0323d41a16c
ahjohn019 <yewrui@hotmail.com>
2022-07-04 22:36:00 +0800

    Update code one

--- reward_20220702.py
+++ reward_20220702.py
@@ -1,11 +1,11 @@
 from curses import keyname
-import pandas as pd 
+import pandas as pd
 import requests
 import re
 import api_keys
 
 # reading the growave csv file
-gw = pd.read_csv("reward_points_june_2022_test.csv")
+gw = pd.read_csv("reward_points_june_2022_test_two.csv")
 
 # reading the shopify csv file
 sp = pd.read_csv("trans_export_one_test.csv")
@@ -16,75 +16,114 @@ API_PWD = api_keys.API_PWD
 SHOP_NAME = api_keys.SHOP_NAME
 
 try:
-    discountCodeApi= []
+    discountCodeApi = []
     date = "2022-06-01"
-    getOrderJson = requests.get("https://"+API_ID+":"+API_PWD+"@"+SHOP_NAME+"/admin/api/2021-07/orders.json?fields=id,discount_codes,name&updated_at_min="+ date +"T15:57:11-04:00&status=any&limit=250")
+    getOrderJson = requests.get("https://"+API_ID+":"+API_PWD+"@"+SHOP_NAME +
+                                "/admin/api/2021-07/orders.json?fields=id,discount_codes,name&updated_at_min=" + date + "T15:57:11-04:00&status=any&limit=250")
     for o in getOrderJson.json()['orders']:
         if(len(o['discount_codes']) > 0):
-            details = {'discount_code':o['discount_codes'][0]['code'].lower(), 'name':o['name'].split("#")[1] }
+            details = {'discount_code': o['discount_codes'][0]['code'].lower(
+            ), 'name': o['name'].split("#")[1]}
             discountCodeApi.append(details)
 
-    #declare point split function
+    # declare point split function
     def pointsSplits(getPoints):
         return getPoints.split()[1]
 
-    #Initialize the value
+    # split the brackets in string
+    def splitBrackets(points, index):
+        convertPoints = points.split('(', 1)[1].split(')')[0]
+        gw.loc[index, 'Coupon'] = convertPoints
+
+    # Initialize the value
     oldOrderIndex = gw[gw['Action'].str.contains('Place')]['Action']
-    containPlusPoints= gw[gw['Points'].str.contains('\+')]['Points']
+    containPlusPoints = gw[gw['Points'].str.contains('\+')]['Points']
     containMinusPoints = gw[gw['Points'].str.contains('-')]['Points']
 
     orderAppend = []
 
     def orderNumberPlaced():
-        #1. Extract the place order number index from action columns, update the column (done)
+        # 1. Extract the place order number index from action columns, update the column (done)
         for index, action in oldOrderIndex.iteritems():
             convertNumber = re.search(r'[\d]{4,5}', action)
-            gw.loc[index, 'Order Name'] = convertNumber.group(0) 
+            gw.loc[index, 'Order Name'] = convertNumber.group(0)
             orderAppend.append(convertNumber.group(0))
 
     def extractEarnPoints():
-        #2. Extract earn points and update to earn column
+        # 2. Extract earn points and update to earn column
         for index, points in containPlusPoints.iteritems():
             gw.loc[index, 'Earn'] = pointsSplits(points)
 
     def extractCoupon():
-        #3. Extract the coupon number and minus from the flexible points columns, update column
+        # 3. Extract the coupon number and minus from the flexible points columns, update column
         for index, points in containMinusPoints.iteritems():
-            convertPoints = points.split('(', 1)[1].split(')')[0]    
+            if '(' in points and ')' in points:
+                splitBrackets(points, index)
+
             gw.loc[index, 'Spend'] = pointsSplits(points)
-            gw.loc[index, 'Coupon'] = convertPoints
+
+    def getStringCoupon():
+        # Extra. Split the string bracket if the value is string
+        for index, points in gw['Points'].iteritems():
+            isString = re.search('[a-zA-Z]', pointsSplits(points))
+            if isString is not None:
+                splitBrackets(points, index)
+                gw.loc[index, 'Spend'] = None
 
     def matchedFlexPoints():
-        #4. Match the flexible points (spend points) with the order api by coupon
+        # 4. Match the flexible points (spend points) with the order api by coupon
         excludeNull = gw[gw['Coupon'].notnull()]['Coupon']
         for index, coupon in excludeNull.iteritems():
-            for b in discountCodeApi:  
-                if coupon.lower() == b['discount_code']:
-                    gw.loc[index, 'Order Name'] = b['name']
-                    orderAppend.append(b['name'])
+            getCouponFiltered = list(filter(
+                lambda discount: discount['discount_code'] == coupon.lower(), discountCodeApi))
+            if getCouponFiltered:
+                gw.loc[index, 'Order Name'] = getCouponFiltered[0]['name']
+                orderAppend.append(getCouponFiltered[0]['name'])
 
     def migrateData():
-        #5. Migrate data from reward points to shopify existing file
+        # 5. Migrate data from reward points to shopify existing file
         for index, name in sp['Name'].iteritems():
-            splitName = name.split('#')[1]        
+            splitName = name.split('#')[1]
             if splitName in orderAppend:
-                sp.loc[index, 'Action'] = gw.loc[gw['Order Name'] == splitName]['Action'].values[0]
-                sp.loc[index, 'Earn'] = gw.loc[gw['Order Name'] == splitName]['Earn'].values[0]
-                sp.loc[index, 'Spend'] = gw.loc[gw['Order Name'] == splitName]['Spend'].values[0]
+                if(len(gw.loc[gw['Order Name']
+                              == splitName]['Action'].values)) >= 2:
+                    sp.loc[len(sp)] = [
+                        sp.loc[index, 'Order'],
+                        sp.loc[index, 'Name'],
+                        sp.loc[index, 'Kind'],
+                        sp.loc[index, 'Gateway'],
+                        sp.loc[index, 'Created At'],
+                        sp.loc[index, 'Status'],
+                        sp.loc[index, 'Amount'],
+                        sp.loc[index, 'Currency'],
+                        sp.loc[index, 'Card Type'],
+                        gw.loc[gw['Order Name']
+                               == splitName]['Action'].values[1],
+                        gw.loc[gw['Order Name']
+                               == splitName]['Earn'].values[1],
+                        gw.loc[gw['Order Name']
+                               == splitName]['Spend'].values[1]
+                    ]
+
+                sp.loc[index, 'Action'] = gw.loc[gw['Order Name']
+                                                 == splitName]['Action'].values[0]
+                sp.loc[index, 'Earn'] = gw.loc[gw['Order Name']
+                                               == splitName]['Earn'].values[0]
+                sp.loc[index, 'Spend'] = gw.loc[gw['Order Name']
+                                                == splitName]['Spend'].values[0]
 
     orderNumberPlaced()
     extractEarnPoints()
     extractCoupon()
+    getStringCoupon()
     matchedFlexPoints()
     migrateData()
 
     sp.to_csv("trans_export_one_test.csv", index=False)
-    gw.to_csv("reward_points_june_2022_test.csv", index=False)
+    gw.to_csv("reward_points_june_2022_test_two.csv", index=False)
+    print('Export Successfully')
+    print(sp)
 
 except Exception as e:
     print(e)
     pass
-
-
-
-    
